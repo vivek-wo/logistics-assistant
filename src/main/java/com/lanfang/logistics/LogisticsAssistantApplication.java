ac9ca797b4801a9be36546fee8b67763365b2194
vivek-wo <vivek.wei.wu@gmail.com>
2021-02-06 16:40:34 +0800

    Initial commit

---
+++ src/main/java/com/lanfang/logistics/LogisticsAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.lanfang.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lanfang/logistics/LogisticsAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanfang.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
