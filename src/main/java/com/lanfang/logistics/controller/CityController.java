41df36b863426775c32550764a5ceb8ff7bdafe6
vivek-wo <vivek.wei.wu@gmail.com>
2021-04-08 17:30:54 +0800

    开发接口

--- src/main/java/com/lanfang/logistics/controller/CityController.java
+++ src/main/java/com/lanfang/logistics/controller/CityController.java
@@ -1,14 +1,21 @@
 package com.lanfang.logistics.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.lanfang.logistics.entity.City;
+import com.lanfang.logistics.service.ICityService;
 import com.lanfang.logistics.vo.CityVo;
+import com.lanfang.logistics.vo.ResultVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,16 +31,73 @@ import java.util.List;
 @RequestMapping("/api")
 public class CityController {
 
+    private final ICityService cityService;
+
+    public CityController(ICityService cityService) {
+        this.cityService = cityService;
+    }
+
     @ApiOperation("获取省份")
     @GetMapping("/v0/city/getProvinceList")
-    public List<CityVo> getProvinceList() {
-        return null;
+    public ResultVo<List<CityVo>> getProvinceList() {
+        QueryWrapper<City> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("province,COUNT(*) AS count");
+        queryWrapper.groupBy("province");
+        List<City> cityList = this.cityService.list(queryWrapper);
+        List<CityVo> resultList = new ArrayList<>();
+        if (cityList != null && !cityList.isEmpty()) {
+            for (City city : cityList) {
+                CityVo cityVo = new CityVo();
+                BeanUtils.copyProperties(city, cityVo);
+                resultList.add(cityVo);
+            }
+        }
+        return ResultVo.successWidthBody(resultList);
     }
 
     @ApiOperation("获取省份")
     @GetMapping("/v0/city/getCityByProvince")
-    public List<CityVo> getCityByProvince() {
-        return null;
+    public ResultVo<List<CityVo>> getCityByProvince(String province) {
+        QueryWrapper<City> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("city,COUNT(*) AS count");
+        if (province != null) {
+            queryWrapper.eq("province", province);
+        }
+        queryWrapper.isNotNull("city");
+        queryWrapper.groupBy("city");
+        List<City> cityList = this.cityService.list(queryWrapper);
+        List<CityVo> resultList = new ArrayList<>();
+        if (cityList != null && !cityList.isEmpty()) {
+            for (City city : cityList) {
+                CityVo cityVo = new CityVo();
+                BeanUtils.copyProperties(city, cityVo);
+                resultList.add(cityVo);
+            }
+        }
+        return ResultVo.successWidthBody(resultList);
+    }
+
+    @ApiOperation("获取区")
+    @GetMapping("/v0/city/getDistrict")
+    public ResultVo<List<CityVo>> getDistrict(String province, String city) {
+        QueryWrapper<City> queryWrapper = new QueryWrapper<>();
+        if (province != null) {
+            queryWrapper.eq("province", province);
+        }
+        if (city != null) {
+            queryWrapper.eq("city", city);
+        }
+        queryWrapper.isNotNull("district");
+        List<City> cityList = this.cityService.list(queryWrapper);
+        List<CityVo> resultList = new ArrayList<>();
+        if (cityList != null && !cityList.isEmpty()) {
+            for (City c : cityList) {
+                CityVo cityVo = new CityVo();
+                BeanUtils.copyProperties(c, cityVo);
+                resultList.add(cityVo);
+            }
+        }
+        return ResultVo.successWidthBody(resultList);
     }
 
 
